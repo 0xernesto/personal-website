882917b66888d158e272c36f8648393258bf663b
0xernesto <ernesto1034@yahoo.com>
2023-10-21 03:31:09 -0500

    modify home page and bio

--- content/bio.md
+++ content/bio.md
@@ -1,8 +1,8 @@
-Hi, I'm Ernesto. I'm dedicating my life to innovative, entrepreneurial initiatives with the hope of enhancing global living standards, boosting economic prosperity and freedom, or addressing inefficiencies in existing societal systems.
+Hi, I'm Ernesto. I like to work on projects that can enhance global living standards, boost economic freedom, address inefficiencies in societal systems, or mitigate existential threats.
 
 I'm currently spending a lot of time learning math, reading books, and building open-source projects. However, the ultimate goal is to found a successful [positive-sum](https://en.wiktionary.org/wiki/positive-sum) startup.
 
-Some of my recent work includes building [Ax Protocol](https://www.ax.finance), which powers the [USX](https://docs.ax.finance/overview/usx) cross-chain native stablecoin, and cofounding [Pelleum](https://ernestor.xyz/projects/pelleum), a pseudonymous platform for reading, writing, and sharing investment content.
+Some of my recent work includes building [Ax Protocol](https://www.ax.finance), which powers the [USX](https://docs.ax.finance/overview/usx) cross-chain-native stablecoin, and cofounding [Pelleum](https://ernestor.xyz/projects/pelleum), a pseudonymous platform for reading, writing, and sharing investment content.
 
 Previously I worked in the aerospace industry — first as a flight test engineer at [Textron Aviation](https://en.wikipedia.org/wiki/Textron_Aviation), then as a manufacturing operations engineer at [Woodward](https://en.wikipedia.org/wiki/Woodward,_Inc.).
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
+import Link from "next/link";
+
 const Home = () => {
 	return (
 		<div className="flex flex-col items-center justify-start w-full flex-1">
 			<div className="flex flex-col items-center justify-center max-w-[1035px] w-11/12 h-fit">
 				<h1 className="text-3xl font-bold mb-5">Ernesto Ramirez</h1>
 				<p className="self-start mb-5">
-					Hello! I'm passionate about accelerating technological
-					advancement that can enhance global living standards, boost
-					economic prosperity and freedom, or address inefficiencies
-					in existing societal systems.
+					Hello! I love engineering and startups!
 				</p>
 				<p className="self-start mb-5">
 					I currently work in the{" "}
@@ -19,19 +18,17 @@ const Home = () => {
 					>
 						DeFi
 					</a>{" "}
-					space. I have extensive experience architecting protocols
-					from the ground up and building across the entire tech
-					stack. Recently, I co-founded {"  "}
-					<a
-						className="underline underline-offset-2 hover:text-white"
-						href="https://chainrule.io/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Chain Rule Labs
-					</a>
-					, with a focus on addressing some of Web3's core challenges
-					to pave the way for future adoption.
+					space, where I've architected and built multiple protocols
+					from the ground up, across the entire tech stack.
+				</p>
+				<p className="self-start mb-5">
+					Feel free to {"  "}
+					<Link href={"/contact"}>
+						<a className="underline underline-offset-2 hover:text-white">
+							reach out
+						</a>
+					</Link>{" "}
+					if we have similar interests!
 				</p>
 				<p className="self-start">Thanks for stopping by!</p>
 			</div>
